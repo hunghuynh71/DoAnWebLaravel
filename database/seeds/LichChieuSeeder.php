fb36c6bcaa779d73ea92331f55593761171dd350
HuynhHoangHung-CDTH18PMC <huynhhoanghung374@gmail.com>
2021-01-06 19:37:17 +0700

    fix loi trang lich chieu

--- database/seeds/LichChieuSeeder.php
+++ database/seeds/LichChieuSeeder.php
@@ -14,7 +14,7 @@ class LichChieuSeeder extends Seeder
     {
         $lc=new LichChieu();
         $lc->phim_id=1;
-        $lc->khung_tg_chieu_id=1;
+        $lc->ktgc_id=1;
         $lc->rap_id=1;
         $lc->dinh_dang_id=1;
         $lc->ngay_chieu='2020-12-31';
