cab143de75e4a9605ed27ffb6f57c4b2aeb4fad7
NguyenChiBinh <73417169+NguyenChiBinh@users.noreply.github.com>
2021-01-14 17:56:09 +0700

    Update khachdatve.php
    .

--- routes/khachdatve.php
+++ routes/khachdatve.php
@@ -11,7 +11,6 @@ Route::post('ThongTinChonGhe','KhachDatVeAPI@Get_Infor_ChonGhe');
 
 Route::post('ThongTinThanhToan','KhachDatVeAPI@Get_Infor_To_ThanhToan');
 
-<<<<<<< HEAD
 Route::post('ThanhToan','KhachDatVeAPI@APIThanhToan');
 
 Route::post('LayKTGChieu','KhachDatVeAPI@Get_KhungTGChieu');
@@ -20,5 +19,3 @@ Route::post('BinhLuanApi','KhachDatVeAPI@BinhLuanApi');
 
 
 Route::get('demoha','KhachDatVeAPI@demo');
-=======
->>>>>>> c6d9eb69f4d4644dfc4717ba3f90a205e057d108
